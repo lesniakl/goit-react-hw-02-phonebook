bf82cf0cc6a0a4e1688359afbd7bd4bb5251300e
Łukasz Leśniak <lukaszlesniaksw@gmail.com>
2023-12-02 01:00:42 +0100

    Add ContactList component

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,43 @@
+import Contact from 'components/Contact/Contact';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class ContactList extends Component {
+  render() {
+    return (
+      <ul>
+        {this.props.contacts.map(contact => {
+          if (!this.props.filter) {
+            return (
+              <Contact
+                key={contact.id}
+                name={contact.name}
+                number={contact.number}
+                onDelete={this.props.onDelete}
+              />
+            );
+          }
+          const filterLower = this.props.filter.toLowerCase();
+          const nameLower = contact.name.toLowerCase();
+          if (nameLower.includes(filterLower)) {
+            return (
+              <Contact
+                key={contact.id}
+                name={contact.name}
+                number={contact.number}
+                onDelete={this.props.onDelete}
+              />
+            );
+          }
+          return null;
+        })}
+      </ul>
+    );
+  }
+}
+
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(PropTypes.object),
+  filter: PropTypes.string,
+  onDelete: PropTypes.func.isRequired,
+};
